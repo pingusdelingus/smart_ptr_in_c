c7a2df12f51001dbf79319a7d079f5c53aa8565a
pingusdelingus <alphaspammail2016@gmail.com>
2026-02-27 16:41:42 -0500

    fixed mem leak

--- src/smart_ptr.c
+++ src/smart_ptr.c
@@ -1,119 +1,106 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <assert.h>
-#include <sys/mman.h>
 #include "smart_ptr.h"
+#include <assert.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-
+/*
+ * old
 smart_ptr* CreateSmartPtr(u32 s)
 {
-  
-    smart_ptr* p = (smart_ptr * ) mmap(NULL, sizeof(smart_ptr), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS ,-1,0 ); 
-    if (p == MAP_FAILED){
+
+    smart_ptr* p = (smart_ptr * ) mmap(NULL, sizeof(smart_ptr), PROT_READ |
+PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS ,-1,0 ); if (p == MAP_FAILED){
     perror("failed memory mapping");
     return NULL;
   }
 
-  p->data = (void *) mmap(NULL, s, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS ,-1,0 ); 
-  
+  p->data = (void *) mmap(NULL, s, PROT_READ | PROT_WRITE, MAP_PRIVATE |
+MAP_ANONYMOUS ,-1,0 );
+
   if (p->data == MAP_FAILED){
     perror("failed memory mapping");
     munmap(p);
     return NULL;
   }
 
-  p->size = s; 
+  p->size = s;
   p->refcount = 1;
 
   return p;
 }// end of CreateSmartPtr
+*/
 
-
+/*
 inline void sdestructor(smart_ptr* p)
 {
   munmap(p->data, data->size);
   munmap(p, sizeof(p));
 }
-
+*/
 
 // called everytime variable leaves scope
-inline void decref(smart_ptr* ptr ) 
+inline void decref(smart_ptr **p)
 {
-  ptr->refcount--;
-  
-  if (refcount == 0 ) { 
-      (ptr);
-  }
-}// end of decref
+    (*p)->refcount--;
 
+    if ((*p)->refcount == 0) {
+        sdestructor(*p);
+    }
+}
 
-inline static smart_ptr *get_s(void *ptr) 
+// end of decref
+
+inline static smart_ptr *get_s(smart_ptr *p) { return p->ptr; } // end of gets
+
+__attribute__((malloc)) void *smalloc(u32 size, void (*dtor)(smart_ptr *))
 {
-    return ptr - sizeof (smart_ptr) + 1;
-}// end of gets
+    smart_ptr *p = malloc(sizeof(smart_ptr));
 
-
-__attribute__((malloc))
-void *smalloc(size_t size, void (*dtor)(void *)) 
-{
-    smart_ptr *meta = malloc(sizeof ( smart_ptr) );
-    
-    *meta = (smart_ptr) {
+    *p = (smart_ptr){
         .dtor = dtor,
         .refcount = 1,
-        .ptr  = malloc(size) // add refcount padding
+        .ptr = malloc(size) // add refcount padding
     };
-    return meta->ptr;
-}// end of smalloc
 
-inline void* sdestructor(void * p)
+    return p;
+} // end of smalloc
+
+inline void sdestructor(smart_ptr *p)
 {
-  free(p->ptr);
-  free(p);
+    free(p->ptr);
+    free((smart_ptr *)p);
 }
 
-
-void sfree(void *ptr) 
+void sfree(smart_ptr *ptr)
 {
-    if (ptr == NULL)
+    if (ptr == NULL) {
         return;
+    }
     smart_ptr *meta = get_s(ptr);
     assert(ptr == meta->ptr); // ptr will be a pointer returned by smalloc
-    
-}// end of sfree
 
+} // end of sfree
 
-inline void incref(smart_ptr* ptr ) 
+inline void incref(smart_ptr *ptr) { ptr->refcount++; }
+
+inline smart_ptr *move(smart_ptr *ptr)
 {
-  ptr->refcount++;
-}
+    if (ptr) {
+        incref(ptr);
+    }
+    return ptr;
+} // smart_ptr*
 
-
-inline smart_ptr* move(smart_ptr* ptr,..)
-{
-  if (ptr) incref(ptr);
-  return ptr;
-}// smart_ptr* 
-
-
-void my_variadic_function(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args); 
-    va_end(args);
-}
-
+typedef struct {
+    f32 x;
+    f32 y;
+} vec2;
 
 int main(void)
-
 {
-  
-  smart_ptr* ptr = CreateSmartPtr(sizeof(vec2));
-
-  
-
-  return 0;
+    smart smart_ptr *my_s_ptr = smalloc(20, &sdestructor);
+    return 0;
 }
--- src/smart_ptr.h
+++ src/smart_ptr.h
@@ -1,49 +1,46 @@
 #include "types.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
+struct smart_ptr;
 
-typedef struct {
-    void (*dtor)(void *);
+struct smart_ptr {
+    void (*dtor)(struct smart_ptr *);
     u16 refcount;
     void *ptr;
-}smart_ptr ;
+};
 
+typedef struct smart_ptr smart_ptr;
 
-static smart_ptr *get_s(void *ptr);
+static smart_ptr *get_s(smart_ptr *ptr);
 // constructor
-void* smalloc(u32 size);
+void *smalloc(u32 size, void (*f)(smart_ptr *));
 
 // destructor
-void sfree(smart_ptr* ptr);
+void sfree(smart_ptr *ptr);
 
-void sdestructor(smart_ptr* p)
-//accessor 
-
-
+void sdestructor(smart_ptr *p);
+// accessor
 
 // wtf is this garbage
-typedef void (*variadic_func_ptr)(const char*, ...);
-
-
+typedef void (*variadic_func_ptr)(const char *, ...);
 
 // move semantics on cleanup
-void decref(smart_ptr* ptr);
-void incref(smart_ptr* ptr);
-smart_ptr* move(smart_ptr* ptr, variadic_func_ptr);
+void decref(smart_ptr **ptr);
+void incref(smart_ptr *ptr);
+smart_ptr *move(smart_ptr *ptr);
 
-
-void variadic_func(const char* format, ...) ;
-
+void variadic_func(const char *format, ...);
 
 #define smart __attribute__((cleanup(decref)))
 
-
 // assignment operator
-#define set(dst, src) do { \
-    void *temp_src = (src);    \
-    if (temp_src) incref(temp_src); \
-    if (dst) decref(dst);      \
-    (dst) = temp_src;          \
-} while (0)
-
+#define set(dst, src)                                                          \
+    do {                                                                       \
+        void *temp_src = (src);                                                \
+        if (temp_src)                                                          \
+            incref(temp_src);                                                  \
+        if (dst)                                                               \
+            decref(dst);                                                       \
+        (dst) = temp_src;                                                      \
+    } while (0)
